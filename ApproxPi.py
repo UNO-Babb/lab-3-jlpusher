45909cd12a1e3113ffa20290e44944f9d838c931
jlpusher <jlpusher@unomaha.edu>
2025-02-10 01:05:54 +0000

    complete

--- ApproxPi.py
+++ ApproxPi.py
@@ -1,7 +1,7 @@
 #ApproxPi.py
-#Name:
-#Date:
-#Assignment:
+#Name:Jessica Pusher
+#Date: 2/9/25
+#Assignment: Approx Pi extra credit
 import math
 import time
 
@@ -16,11 +16,21 @@ def main():
   start = time.time()
   #calculate pi using the approximation technique
   #Loop until the level of percision is reached
+  approxPi = 4/1
+  sign = -1
+  denom = 3
+
+  while round(approxPi, 5) != round(realPi, 5):
+    #print (approxPi)
+    approxPi = approxPi + (sign * 4/denom)
+
+    sign = sign * -1
+    denom = denom + 2
 
   end = time.time()
 
   elapsedTime = end - start
-  print(elapsedTime)
+  print("Elapsed Time = ", elapsedTime)
 
 if __name__ == '__main__':
   main()
